0acb96b95ff0db00ca4e290bd203afedebe7fe9e
John Bielejeski <jmbielejeski@gmail.com>
2021-03-31 11:30:59 -0700

    added database sql text and pet hotel py file

---
+++ database.sql
@@ -0,0 +1,35 @@
+-- CREATE DATABASE "pet_hotel";
+
+CREATE TABLE "owners" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) UNIQUE NOT NULL
+);
+
+
+
+CREATE TABLE "pets" (
+    "id" SERIAL PRIMARY KEY,
+    "owner_id" INT REFERENCES "owners",
+    "name" VARCHAR (80) NOT NULL,
+    "breed" VARCHAR (100) NOT NULL,
+    "color" VARCHAR (80) NOT NULL,
+    "checked_date" DATE DEFAULT NULL,
+    "checked_in" BOOLEAN NOT NULL
+);
+
+INSERT INTO "owners" ("name")
+VALUES ('John B'), ('John S'), ('George'), ('Jason');
+
+INSERT INTO "pets" ("owner_id", "name", "breed", "color", "checked_date", "checked_in")
+VALUES 
+(1, 'Fluffy', 'Golden Retriever', 'Gold', NULL, TRUE),
+(1, 'Snuffy', 'Hamster', 'Brown', NULL, TRUE),
+(2, 'Bingo', 'Fish', 'Orange', NULL, TRUE),
+(2, 'Slinky', 'Snake', 'Black', NULL, TRUE),
+(3, 'Jack', 'Rabbit', 'White', NULL, TRUE),
+(3, 'Sher Khan', 'Tiger', 'Orange', NULL, TRUE),
+(4, 'Sparkles', 'Unicorn', 'Rainbow', NULL, TRUE),
+(4, 'Flipper', 'Dolphin', 'Grey', NULL, TRUE);
+
+
+-- CREATE TABLE "petowner"
---
+++ pet_hotel.py
@@ -0,0 +1,42 @@
+import flask
+import psycopg2
+from flask import request, jsonify, make_response
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+import os
+
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# default route to show home page and history table
+@app.route('/api/pets/all', methods=['GET'])
+def api_all():
+  # if we can get the data back and then even print it 
+    connection = psycopg2.connect(user=DATABASE_USERNAME,
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="pet_hotel")
+    cursor = connection.cursor()
+    postgresSQL_select_Query = "SELECT * FROM pets"
+    
+    # Executes query
+    cursor.execute(postgresSQL_select_Query)
+    pets = cursor.fetchall()
+
+    return jsonify(pets)
+
+app.run()
+
+# @app.route('/api/pets/add', methods=['POST'])
+# def api_add():
+#   print(request)
+#   pet = request.form['pet']
+
+#   try:
+
+
+# export default FLASK_APP=pet_hotel.py
+# flask run 
+# python3 pet_hotel.py
